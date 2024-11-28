59e1a7d0265221b597f3e48821e01ae4b4117604
Hridaydeep <64785471+Hridaydeep@users.noreply.github.com>
2024-11-28 11:20:05 +0530

    Add files via upload
    Added Loan service

---
+++ 2. Microservices/Project 1/loans/loans/src/main/java/com/eazybytes/loans/service/ILoansService.java
@@ -0,0 +1,15 @@
+package com.eazybytes.loans.service;
+
+import com.eazybytes.loans.dto.LoansDto;
+import org.springframework.stereotype.Service;
+
+
+public interface ILoansService {
+    void createLoan(String mobileNumber);
+
+    LoansDto fetchLoan(String mobileNumber);
+
+    boolean deleteLoan(String mobileNumber);
+
+    boolean updateLoan(LoansDto loansDto);
+}
